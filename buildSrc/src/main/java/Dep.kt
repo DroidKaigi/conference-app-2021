371c73297039861b477cceb5d077e7da5a3b7a7b
takahirom <takam.dev@gmail.com>
2020-12-28 18:12:24 +0900

    Support ios in model and data

--- buildSrc/src/main/java/Dep.kt
+++ buildSrc/src/main/java/Dep.kt
@@ -2,7 +2,8 @@ package io.github.droidkaigi.confsched2021.news
 
 object Dep {
     val klock = "com.soywiz.korlibs.klock:klock:2.0.0-alpha-1.4.0-rc"
-    val ktor = "io.ktor:ktor-client:1.3.2-1.4.0-rc"
-    val ktorJson = "io.ktor:ktor-client-json:1.3.2-1.4.0-rc"
-    val ktorSerialization = "io.ktor:ktor-client-serialization:1.3.2-1.4.0-rc"
+    val ktor = "io.ktor:ktor-client-core:1.5.0"
+    val ktorJson = "io.ktor:ktor-client-json:1.5.0"
+    val ktorSerialization = "io.ktor:ktor-client-serialization:1.5.0"
+    val ktorMock = "io.ktor:ktor-client-mock:1.5.0"
 }
