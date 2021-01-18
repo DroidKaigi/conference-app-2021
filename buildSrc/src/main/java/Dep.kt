91cfc4aedf043c198ac04971886247b4e56827a9
takahirom <takam.dev@gmail.com>
2021-01-18 10:14:30 +0900

    Partially introduce dep.kt

--- buildSrc/src/main/java/Dep.kt
+++ buildSrc/src/main/java/Dep.kt
@@ -1,9 +1,35 @@
 package io.github.droidkaigi.confsched2021.news
 
 object Dep {
-    val klock = "com.soywiz.korlibs.klock:klock:2.0.0-alpha-1.4.0-rc"
-    val ktor = "io.ktor:ktor-client-core:1.5.0"
-    val ktorJson = "io.ktor:ktor-client-json:1.5.0"
-    val ktorSerialization = "io.ktor:ktor-client-serialization:1.5.0"
-    val ktorMock = "io.ktor:ktor-client-mock:1.5.0"
+    object Coroutines {
+        const val bom = "org.jetbrains.kotlinx:kotlinx-coroutines-bom:1.4.2"
+        const val core = "org.jetbrains.kotlinx:kotlinx-coroutines-core"
+    }
+
+    object Serialization {
+        const val core = "org.jetbrains.kotlinx:kotlinx-serialization-core:1.0.1"
+    }
+
+    object KotlinTest {
+        const val common = "org.jetbrains.kotlin:kotlin-test-common"
+        const val annotationCommon = "org.jetbrains.kotlin:kotlin-test-annotations-common"
+        const val junit = "org.jetbrains.kotlin:kotlin-test-junit"
+    }
+
+    const val klock = "com.soywiz.korlibs.klock:klock:2.0.3"
+
+    object Ktor {
+        const val bom = "io.ktor:ktor-bom:1.5.0"
+        const val core = "io.ktor:ktor-client-core"
+        const val json = "io.ktor:ktor-client-json"
+        const val okHttp = "io.ktor:ktor-client-okhttp"
+        const val serialization = "io.ktor:ktor-client-serialization"
+        const val mock = "io.ktor:ktor-client-mock"
+    }
+
+    object Dagger {
+        const val hiltAndroid = "com.google.dagger:hilt-android:2.31-alpha"
+        const val hiltAndroidTesting = "com.google.dagger:hilt-android-testing:2.31-alpha"
+        const val hiltAndroidCompiler = "com.google.dagger:hilt-android-compiler:2.31-alpha"
+    }
 }
