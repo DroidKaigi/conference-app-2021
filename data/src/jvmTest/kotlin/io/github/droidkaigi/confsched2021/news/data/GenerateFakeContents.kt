ec863c721cf4779651909f340c0d945cabe5befd
takahirom <takam.dev@gmail.com>
2021-01-26 09:31:48 +0900

    Add Fake content

---
+++ data/src/jvmTest/kotlin/io/github/droidkaigi/confsched2021/news/data/GenerateFakeContents.kt
@@ -0,0 +1,46 @@
+package io.github.droidkaigi.confsched2021.news.data
+
+import com.squareup.kotlinpoet.metadata.KotlinPoetMetadataPreview
+import kotlinx.datetime.Instant
+import kotlin.test.Test
+
+class GenerateFakeContents {
+    @OptIn(KotlinPoetMetadataPreview::class)
+    @Test
+    fun generate() = runBlocking {
+        val newsList = fakeNewsApi()
+            .fetch()
+        val format = build(newsList)
+
+        println(format)
+    }
+
+    fun build(value: Any): String {
+        return when (value) {
+            is List<*> -> {
+                value.joinToString { build(it!!) }
+            }
+            is Number -> {
+                value.toString()
+            }
+            is String -> {
+                "\"\"\"" + value.toString() + "\"\"\""
+            }
+            is Instant -> {
+                "Instant.fromEpochMilliseconds(" + value.toEpochMilliseconds() + ")"
+            }
+            else -> {
+                val kClass = value::class
+                val primaryConstructor = kClass.constructors.toList()[0]
+                kClass.qualifiedName + "(" +
+                    primaryConstructor.parameters.joinToString(",") { parameter ->
+                        parameter.name + "=" + build(
+                            kClass.members.first { member ->
+                                parameter.name == member.name
+                            }.call(value)!!
+                        )
+                    } + ")"
+            }
+        }
+    }
+}
