f1d1587ecd564a06a2e284e5e4932bc41cd28360
takahirom <takam.dev@gmail.com>
2021-02-05 09:17:41 +0900

    Fix and separate data module

---
+++ data/api/src/commonTest/kotlin/io/github/droidkaigi/confsched2021/news/data/ApiTest.kt
@@ -0,0 +1,10 @@
+package io.github.droidkaigi.confsched2021.news.data
+
+import kotlin.test.Test
+import kotlin.test.assertTrue
+
+class ApiTest {
+    @Test
+    fun test() {
+    }
+}
---
+++ data/repository/src/iosTest/kotlin/io/github/droidkaigi/confsched2021/news/data/TestUtil.kt
@@ -0,0 +1,3 @@
+package io.github.droidkaigi.confsched2021.news.data
+
+actual fun runBlocking(block: suspend () -> Unit) = kotlinx.coroutines.runBlocking { block() }
