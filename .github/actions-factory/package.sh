e2d471006e8e309e87dbb30ed381f891eef503aa
Jumpei Matsuda <jmatsu.drm@gmail.com>
2021-02-14 16:46:22 +0900

    chore(ci): Adds an action to commit a deployment status
    - Separate staging stuff and use the default branch-based triggers

--- .github/actions-factory/package.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-npm run build
-
-while read action_name; do
-  ncc build -o "actions/${action_name//_/-}/dist" --source-map "lib/actions/$action_name/index.js"
-done < <(ls -1 src/actions)
