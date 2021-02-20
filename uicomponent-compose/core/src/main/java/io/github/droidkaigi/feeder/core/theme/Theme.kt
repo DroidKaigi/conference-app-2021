907023d7d0d0f45cebbb0ef5dc40c0ca8eb7070a
takahirom <takam.dev@gmail.com>
2021-02-20 17:49:25 +0900

    Delete import

--- uicomponent-compose/core/src/main/java/io/github/droidkaigi/feeder/core/theme/Theme.kt
+++ uicomponent-compose/core/src/main/java/io/github/droidkaigi/feeder/core/theme/Theme.kt
@@ -1,6 +1,5 @@
 package io.github.droidkaigi.feeder.core.theme
 
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
