b0a0af4ce56bf03f4b304daf76f34820d83b47db
RyuNen344 <s1100633@outlook.com>
2021-02-28 14:21:36 +0900

    make authApi non throw exception when server returns 409

--- data/api/src/commonMain/kotlin/io/github/droidkaigi/feeder/data/AuthApi.kt
+++ data/api/src/commonMain/kotlin/io/github/droidkaigi/feeder/data/AuthApi.kt
@@ -3,9 +3,15 @@ package io.github.droidkaigi.feeder.data
 import dev.gitlive.firebase.Firebase
 import dev.gitlive.firebase.auth.auth
 import io.ktor.client.HttpClient
+import io.ktor.client.features.ResponseException
+import io.ktor.client.features.logging.DEFAULT
+import io.ktor.client.features.logging.Logger
+import io.ktor.client.request.header
 import io.ktor.client.request.headers
 import io.ktor.client.request.post
 import io.ktor.http.ContentType
+import io.ktor.http.HttpHeaders
+import io.ktor.http.HttpStatusCode
 import io.ktor.http.contentType
 import kotlinx.coroutines.flow.first
 
@@ -43,12 +49,16 @@ class AuthApi(
     }
 
     private suspend fun registerToServer(createdIdToken: String) {
-        httpClient.post<String>("https://ssot-api-staging.an.r.appspot.com/accounts") {
-            headers {
-                set("Authorization", "Bearer $createdIdToken")
+        runCatching {
+            httpClient.post<String>("https://ssot-api-staging.an.r.appspot.com/accounts") {
+                header(HttpHeaders.Authorization, "Bearer $createdIdToken")
+                contentType(ContentType.Application.Json)
+                body = "{}"
             }
-            contentType(ContentType.Application.Json)
-            body = "{}"
+        }.getOrElse {
+            if (it !is ResponseException || it.response.status != HttpStatusCode.Conflict) {
+                throw it
+            }
         }
     }
 }
