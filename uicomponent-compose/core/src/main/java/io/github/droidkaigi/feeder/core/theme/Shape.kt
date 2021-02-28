ab18526495953108d065f9c802cfa6503ccb0afe
NUmeroAndDev <numero.dev.and@gmail.com>
2021-02-28 11:29:26 +0900

    Define shape theme

--- uicomponent-compose/core/src/main/java/io/github/droidkaigi/feeder/core/theme/Shape.kt
+++ uicomponent-compose/core/src/main/java/io/github/droidkaigi/feeder/core/theme/Shape.kt
@@ -1,11 +1,19 @@
 package io.github.droidkaigi.feeder.core.theme
 
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.shape.CutCornerShape
 import androidx.compose.material.Shapes
 import androidx.compose.ui.unit.dp
 
 val shapes = Shapes(
-    small = RoundedCornerShape(4.dp),
-    medium = RoundedCornerShape(4.dp),
-    large = RoundedCornerShape(0.dp)
+    small = CutCornerShape(
+        topStart = 8.dp,
+        bottomEnd = 8.dp
+    ),
+    medium = CutCornerShape(
+        topStart = 8.dp,
+        bottomEnd = 8.dp
+    ),
+    large = CutCornerShape(
+        topStart = 32.dp
+    )
 )
