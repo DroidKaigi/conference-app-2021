3f680daa3747e72837e50958158415b2895bc179
Dai1678 <dai7774@gmail.com>
2021-02-28 17:04:26 +0900

    run spotlessApply

--- uicomponent-compose/main/src/main/java/io/github/droidkaigi/feeder/DrawerButton.kt
+++ uicomponent-compose/main/src/main/java/io/github/droidkaigi/feeder/DrawerButton.kt
@@ -15,9 +15,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.ColorFilter
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import io.github.droidkaigi.feeder.core.theme.ConferenceAppFeederTheme
 import io.github.droidkaigi.feeder.main.R
