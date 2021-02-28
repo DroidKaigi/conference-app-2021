b84890918a5e2c1baebccdaea07d40cdaf80ca1b
mune0903 <b1016051@fun.ac.jp>
2021-02-28 19:03:17 +0900

    Improve FeedItem accecibility

--- uicomponent-compose/feed/src/main/java/io/github/droidkaigi/feeder/feed/FeedItem.kt
+++ uicomponent-compose/feed/src/main/java/io/github/droidkaigi/feeder/feed/FeedItem.kt
@@ -15,6 +15,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -41,6 +42,7 @@ fun FeedItem(
                 onClick = { onClick(feedItem) }
             )
             .fillMaxWidth()
+            .semantics(mergeDescendants = true) { }
     ) {
         val (media, image, title, date, favorite) = createRefs()
         if (showMediaLabel) {
@@ -74,7 +76,8 @@ fun FeedItem(
                 .width(96.dp)
 //                .aspectRatio(16F / 9F)
                 .aspectRatio(1F / 1F),
-            contentScale = ContentScale.Crop
+            contentScale = ContentScale.Crop,
+            contentDescription = null
         )
         Text(
             modifier = Modifier.constrainAs(title) {
