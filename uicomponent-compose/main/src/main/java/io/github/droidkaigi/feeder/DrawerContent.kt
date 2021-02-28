3c67d69eaaab07206fccd263a011aa1cbe39b6cb
Dai1678 <dai7774@gmail.com>
2021-02-28 12:32:37 +0900

    create Drawer Header layout

--- uicomponent-compose/main/src/main/java/io/github/droidkaigi/feeder/DrawerContent.kt
+++ uicomponent-compose/main/src/main/java/io/github/droidkaigi/feeder/DrawerContent.kt
@@ -1,13 +1,24 @@
 package io.github.droidkaigi.feeder
 
 import androidx.annotation.IdRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.Divider
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -81,7 +92,27 @@ enum class DrawerContents(
 @Composable
 fun DrawerContent(onNavigate: (route: String) -> Unit) {
     Column {
-        Spacer(modifier = Modifier.height(36.dp))
+        Spacer(modifier = Modifier.height(52.dp))
+        Row(
+            horizontalArrangement = Arrangement.Start,
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .padding(horizontal = 24.dp)
+                .fillMaxWidth()
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.ic_logo),
+                contentDescription = "logo"
+            )
+            Spacer(Modifier.width(12.dp))
+            Text(
+                text = "DroidKaigi",
+                modifier = Modifier.fillMaxWidth()
+            )
+        }
+        Spacer(modifier = Modifier.height(20.dp))
+        Divider()
+        Spacer(modifier = Modifier.height(12.dp))
         for (group in DrawerContents.Group.values()) {
             val groupContents = DrawerContents.values()
                 .filter { content -> content.group == group }
