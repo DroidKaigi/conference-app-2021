ea93cb5ee8f13075bf4cdfd7a4b6b7d2a6a22c5a
tick-taku <tick.taku.77@gmail.com>
2021-03-02 02:04:46 +0900

    Define CoroutineScope.

--- uicomponent-compose/other/src/main/java/io/github/droidkaigi/feeder/core/FakeContributeViewModel.kt
+++ uicomponent-compose/other/src/main/java/io/github/droidkaigi/feeder/core/FakeContributeViewModel.kt
@@ -1,14 +1,22 @@
 package io.github.droidkaigi.feeder.core
 
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Runnable
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.receiveAsFlow
+import kotlin.coroutines.CoroutineContext
 
 class FakeContributeViewModel: ContributorViewModel {
 
     private val effectChannel = Channel<ContributorViewModel.Effect>(Channel.UNLIMITED)
     override val effect: Flow<ContributorViewModel.Effect> = effectChannel.receiveAsFlow()
+
+    private val coroutineScope = CoroutineScope(object: CoroutineDispatcher() {
+        override fun dispatch(context: CoroutineContext, block: Runnable) = block.run()
+    })
 
     override val state: StateFlow<ContributorViewModel.State>
         get() = TODO("Not yet implemented")
