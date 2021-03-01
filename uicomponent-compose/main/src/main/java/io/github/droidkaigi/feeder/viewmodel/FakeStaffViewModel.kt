1c3dc44dd4cdaefb6fe1ae49f5b34f7a7c53d02c
Haruhiko Takada <haruhiko.takada@gmail.com>
2021-03-01 22:59:15 +0900

    Implements FakeStaffViewModel

--- uicomponent-compose/main/src/main/java/io/github/droidkaigi/feeder/viewmodel/FakeStaffViewModel.kt
+++ uicomponent-compose/main/src/main/java/io/github/droidkaigi/feeder/viewmodel/FakeStaffViewModel.kt
@@ -1,24 +1,61 @@
 package io.github.droidkaigi.feeder.viewmodel
 
+import io.github.droidkaigi.feeder.AppError
+import io.github.droidkaigi.feeder.Staff
 import io.github.droidkaigi.feeder.fakeStaffs
 import io.github.droidkaigi.feeder.staff.StaffViewModel
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.flow.stateIn
+import kotlin.coroutines.CoroutineContext
 
-fun fakeStaffViewModel() = FakeStaffViewModel()
-class FakeStaffViewModel : StaffViewModel {
-    override val state: StateFlow<StaffViewModel.State> = MutableStateFlow(
-        StaffViewModel.State(
-            showProgress = false,
-            staffContents = fakeStaffs()
-        )
-    )
+fun fakeStaffViewModel(errorFetchData: Boolean = false) = FakeStaffViewModel(errorFetchData)
+class FakeStaffViewModel(errorFetchData: Boolean) : StaffViewModel {
 
     private val effectChannel = Channel<StaffViewModel.Effect>(Channel.UNLIMITED)
     override val effect: Flow<StaffViewModel.Effect> = effectChannel.receiveAsFlow()
+
+    private val coroutineScope = CoroutineScope(
+        object : CoroutineDispatcher() {
+            // for preview
+            override fun dispatch(context: CoroutineContext, block: Runnable) {
+                block.run()
+            }
+        }
+    )
+
+    private val mutableStaffs = MutableStateFlow(
+        fakeStaffs()
+    )
+
+    private val errorStaffs = flow<List<Staff>> {
+        throw AppError.ApiException.ServerException(null)
+    }.catch { error ->
+        effectChannel.send(StaffViewModel.Effect.ErrorMessage(error as AppError))
+    }.stateIn(coroutineScope, SharingStarted.Lazily, fakeStaffs())
+
+    private val mStaffs: StateFlow<List<Staff>> = if (errorFetchData) {
+        errorStaffs
+    } else {
+        mutableStaffs
+    }
+
+    override val state: StateFlow<StaffViewModel.State> = mStaffs.map {
+        StaffViewModel.State(
+            showProgress = false,
+            staffContents = it
+        )
+    }.stateIn(coroutineScope, SharingStarted.Eagerly, StaffViewModel.State())
+
     override fun event(event: StaffViewModel.Event) {
     }
 }
