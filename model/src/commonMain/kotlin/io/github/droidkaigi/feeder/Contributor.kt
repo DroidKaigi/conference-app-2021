dce5a80290be10dd70bf5609fac1d7181879d1ba
tick-taku <tick.taku.77@gmail.com>
2021-03-02 02:00:47 +0900

    Make contributer's fake.

--- model/src/commonMain/kotlin/io/github/droidkaigi/feeder/Contributor.kt
+++ model/src/commonMain/kotlin/io/github/droidkaigi/feeder/Contributor.kt
@@ -6,3 +6,18 @@ data class Contributor(
     val url: String,
     val image: String
 )
+
+fun fakeContributors() = listOf(
+    Contributor(
+        id = "1",
+        name = "Pie",
+        url = "https://developer.android.com/",
+        image = "https://via.placeholder.com/150",
+    ),
+    Contributor(
+        id = "2",
+        name = "Oreo",
+        url = "https://developer.android.com/",
+        image = "https://via.placeholder.com/150",
+    )
+)
