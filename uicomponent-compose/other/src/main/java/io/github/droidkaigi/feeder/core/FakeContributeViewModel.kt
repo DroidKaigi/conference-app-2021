72282365624b32d665ee571a3b2aea673662a72e
tick-taku <tick.taku.77@gmail.com>
2021-03-02 02:06:26 +0900

    Override state property.

--- uicomponent-compose/other/src/main/java/io/github/droidkaigi/feeder/core/FakeContributeViewModel.kt
+++ uicomponent-compose/other/src/main/java/io/github/droidkaigi/feeder/core/FakeContributeViewModel.kt
@@ -1,15 +1,24 @@
 package io.github.droidkaigi.feeder.core
 
+import io.github.droidkaigi.feeder.AppError
+import io.github.droidkaigi.feeder.Contributor
+import io.github.droidkaigi.feeder.fakeContributors
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Runnable
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.flow.stateIn
 import kotlin.coroutines.CoroutineContext
 
-class FakeContributeViewModel: ContributorViewModel {
+class FakeContributeViewModel(private val errorFetchData: Boolean): ContributorViewModel {
 
     private val effectChannel = Channel<ContributorViewModel.Effect>(Channel.UNLIMITED)
     override val effect: Flow<ContributorViewModel.Effect> = effectChannel.receiveAsFlow()
@@ -18,8 +27,22 @@ class FakeContributeViewModel: ContributorViewModel {
         override fun dispatch(context: CoroutineContext, block: Runnable) = block.run()
     })
 
-    override val state: StateFlow<ContributorViewModel.State>
-        get() = TODO("Not yet implemented")
+    private val mutableContributorContents = MutableStateFlow(
+        fakeContributors()
+    )
+    private val errorContributorContents = flow<List<Contributor>> {
+        throw AppError.ApiException.ServerException(null)
+    }
+        .catch { effectChannel.send(ContributorViewModel.Effect.ErrorMessage(it as AppError)) }
+        .stateIn(coroutineScope, SharingStarted.Lazily, fakeContributors())
+
+    private val contributorContents =
+        if (errorFetchData) errorContributorContents
+        else mutableContributorContents
+
+    override val state: StateFlow<ContributorViewModel.State> =
+        contributorContents.map { ContributorViewModel.State(contributorContents = it) }
+            .stateIn(coroutineScope, SharingStarted.Eagerly, ContributorViewModel.State())
 
     override fun event(event: ContributorViewModel.Event) {
         TODO("Not yet implemented")
