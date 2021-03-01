5dbc3d696daf82339a4e99902ca541dd44dbbf3a
SOUP <soupyong@gmail.com>
2021-03-01 23:43:18 +0900

    Keep the splash screen finished through screen rotation

--- uicomponent-compose/main/src/main/java/io/github/droidkaigi/feeder/DroidKaigiApp.kt
+++ uicomponent-compose/main/src/main/java/io/github/droidkaigi/feeder/DroidKaigiApp.kt
@@ -7,7 +7,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
@@ -16,7 +16,7 @@ import io.github.droidkaigi.feeder.core.theme.ConferenceAppFeederTheme
 @Composable
 fun DroidKaigiApp(firstSplashScreenState: SplashState = SplashState.Shown) {
     ConferenceAppFeederTheme {
-        var splashShown by remember {
+        var splashShown by rememberSaveable {
             mutableStateOf(firstSplashScreenState)
         }
         val transition = updateTransition(splashShown)
