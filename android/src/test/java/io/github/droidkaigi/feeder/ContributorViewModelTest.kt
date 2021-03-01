1c7f1f5316f2a49c0165eb65f957bc1d01bd37db
tick-taku <tick.taku.77@gmail.com>
2021-03-02 02:26:22 +0900

    Create ContributorViewModelTest file.

---
+++ android/src/test/java/io/github/droidkaigi/feeder/ContributorViewModelTest.kt
@@ -0,0 +1,10 @@
+package io.github.droidkaigi.feeder
+
+import kotlinx.coroutines.InternalCoroutinesApi
+import org.junit.runner.RunWith
+import org.junit.runners.Parameterized
+
+@InternalCoroutinesApi
+@RunWith(Parameterized::class)
+class ContributorViewModelTest {
+}
