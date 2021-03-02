ba3fef3ffa35b1eb6be84451c743c67afbc375c9
Haruhiko Takada <haruhiko.takada@gmail.com>
2021-03-03 02:25:18 +0900

    Fix build error.

--- uicomponent-compose/main/src/main/java/io/github/droidkaigi/feeder/viewmodel/ProvideRealViewModels.kt
+++ uicomponent-compose/main/src/main/java/io/github/droidkaigi/feeder/viewmodel/ProvideRealViewModels.kt
@@ -4,6 +4,7 @@ import androidx.compose.runtime.Composable
 import androidx.lifecycle.viewmodel.compose.viewModel
 import io.github.droidkaigi.feeder.feed.ProvideFeedViewModel
 import io.github.droidkaigi.feeder.staff.ProvideStaffViewModel
+import io.github.droidkaigi.feeder.staff.fakeStaffViewModel
 
 @Composable
 fun ProvideViewModels(content: @Composable () -> Unit) {
