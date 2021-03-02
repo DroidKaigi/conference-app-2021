004070427eba8c87a6452ae17b8061095827ab70
mahoya <support@maho-ya.com>
2021-03-02 22:50:19 +0900

    Remove border import

--- uicomponent-compose/feed/src/main/java/io/github/droidkaigi/feeder/feed/FeedItem.kt
+++ uicomponent-compose/feed/src/main/java/io/github/droidkaigi/feeder/feed/FeedItem.kt
@@ -1,7 +1,6 @@
 package io.github.droidkaigi.feeder.feed
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.aspectRatio
