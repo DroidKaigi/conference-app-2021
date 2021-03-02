a33a7b84ad76c934109ee12dd08470fd8e909a1d
Takafumi Kanda <kanda.t.web@gmail.com>
2021-03-02 21:28:04 +0900

    Use black scrim color for drawer in all theme

--- uicomponent-compose/main/src/main/java/io/github/droidkaigi/feeder/AppContent.kt
+++ uicomponent-compose/main/src/main/java/io/github/droidkaigi/feeder/AppContent.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.net.Uri
 import androidx.browser.customtabs.CustomTabsIntent
 import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.material.DrawerDefaults
 import androidx.compose.material.DrawerValue
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.ModalDrawer
@@ -13,6 +14,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
@@ -65,7 +67,8 @@ fun AppContent(
                     drawerState.close()
                 }
             }
-        }
+        },
+        scrimColor = Color.Black.copy(alpha = DrawerDefaults.ScrimOpacity),
     ) {
         NavHost(navController, startDestination = "feed/{feedTab}") {
             composable(
