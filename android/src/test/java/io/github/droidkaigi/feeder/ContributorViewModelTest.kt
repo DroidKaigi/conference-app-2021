504896182d748c6b725b250425c2b265becec1bd
tick-taku <tick.taku.77@gmail.com>
2021-03-02 18:21:51 +0900

    Test when error.

--- android/src/test/java/io/github/droidkaigi/feeder/ContributorViewModelTest.kt
+++ android/src/test/java/io/github/droidkaigi/feeder/ContributorViewModelTest.kt
@@ -3,7 +3,10 @@ package io.github.droidkaigi.feeder
 import io.github.droidkaigi.feeder.core.ContributorViewModel
 import io.github.droidkaigi.feeder.core.fakeContributorViewModel
 import io.kotest.matchers.ints.shouldBeGreaterThan
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.types.shouldBeInstanceOf
 import kotlinx.coroutines.InternalCoroutinesApi
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.test.runBlockingTest
 import org.junit.Rule
 import org.junit.Test
@@ -24,6 +27,13 @@ class ContributorViewModelTest(
     fun contents() = coroutineTestRule.testDispatcher.runBlockingTest {
         val contributorViewModel = contributorViewModelFactory.create()
         contributorViewModel.state.value.contributorContents.size shouldBeGreaterThan 1
+    }
+
+    @Test
+    fun errorWhenFetch() = coroutineTestRule.testDispatcher.runBlockingTest {
+        val contributorViewModel = contributorViewModelFactory.create(errorFetchData = true)
+        contributorViewModel.effect.first()
+            .shouldBeInstanceOf<ContributorViewModel.Effect.ErrorMessage>()
     }
 
     class ContributorViewModelFactory(
