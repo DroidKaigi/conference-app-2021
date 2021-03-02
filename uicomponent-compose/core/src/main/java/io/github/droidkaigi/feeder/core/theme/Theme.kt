a8365a48ea605a238a46bcf44ba84ede95444330
Takafumi Kanda <kanda.t.web@gmail.com>
2021-03-02 18:47:23 +0900

    Enable dark theme based on device theme setting

--- uicomponent-compose/core/src/main/java/io/github/droidkaigi/feeder/core/theme/Theme.kt
+++ uicomponent-compose/core/src/main/java/io/github/droidkaigi/feeder/core/theme/Theme.kt
@@ -1,5 +1,6 @@
 package io.github.droidkaigi.feeder.core.theme
 
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
@@ -9,29 +10,18 @@ import dev.chrisbanes.accompanist.insets.ProvideWindowInsets
 private val DarkColorPalette = darkColors(
     primary = blue200,
     primaryVariant = blue700,
-    secondary = green200
+    secondary = green200,
 )
 
 private val LightColorPalette = lightColors(
     primary = blue200,
     primaryVariant = blue700,
-    secondary = green200
-
-    /* Other default colors to override
-background = Color.White,
-surface = Color.White,
-onPrimary = Color.White,
-onSecondary = Color.Black,
-onBackground = Color.Black,
-onSurface = Color.Black,
-*/
+    secondary = green200,
 )
 
 @Composable
 fun ConferenceAppFeederTheme(
-    // Currently dark mode is not supported
-    // Please fix this https://github.com/DroidKaigi/conference-app-2021/issues/38
-    darkTheme: Boolean = false,
+    darkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable
     () -> Unit,
 ) {
