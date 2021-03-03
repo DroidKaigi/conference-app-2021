7e6a9e16d261677aac2c3ce3d8a3c74571875860
shimoju <soba.ha.kenkou@gmail.com>
2021-03-04 08:27:37 +0900

    remove unused imports

--- uicomponent-compose/main/src/main/java/io/github/droidkaigi/feeder/LandingScreen.kt
+++ uicomponent-compose/main/src/main/java/io/github/droidkaigi/feeder/LandingScreen.kt
@@ -10,8 +10,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import io.github.droidkaigi.feeder.core.theme.ConferenceAppFeederTheme
-import io.github.droidkaigi.feeder.feed.ProvideFeedViewModel
-import io.github.droidkaigi.feeder.feed.fakeFeedViewModel
 import io.github.droidkaigi.feeder.main.R
 import kotlinx.coroutines.delay
 
