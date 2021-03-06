050a1c872b802870ab38cbb8d944c90931638874
pluulove <pluulove@gmail.com>
2021-03-06 17:15:00 +0900

    optimize unused import

--- uicomponent-compose/core/src/main/java/io/github/droidkaigi/feeder/core/NetworkImage.kt
+++ uicomponent-compose/core/src/main/java/io/github/droidkaigi/feeder/core/NetworkImage.kt
@@ -3,9 +3,7 @@ package io.github.droidkaigi.feeder.core
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
