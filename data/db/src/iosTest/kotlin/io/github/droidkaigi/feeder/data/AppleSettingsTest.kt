915a41082138f27c927200b1988d2883db2c4a1b
watanavex <y_watanabe@yumemi.co.jp>
2021-03-06 22:37:56 +0900

    Add tests

---
+++ data/db/src/iosTest/kotlin/io/github/droidkaigi/feeder/data/AppleSettingsTest.kt
@@ -0,0 +1,47 @@
+package io.github.droidkaigi.feeder.data
+
+import platform.Foundation.NSUserDefaults
+import kotlin.test.AfterTest
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class AppleSettingsTest {
+
+    private val userDefaults = NSUserDefaults(
+        suiteName = "io.github.droidkaigi.feeder.data.AppleSettingsTest"
+    )
+
+    @AfterTest
+    fun teardown() {
+        userDefaults.dictionaryRepresentation().keys.forEach {
+            userDefaults.removeObjectForKey(it as String)
+        }
+    }
+
+    @Test
+    fun testIntegerReadAndWrite() {
+        // give
+        val key = "integer-key"
+        val appleSettings = AppleSettings(userDefaults)
+
+        // when
+        appleSettings.putInt(key, 22)
+
+        // then
+        assertEquals(appleSettings.getInt(key), 22)
+    }
+
+    @Test
+    fun testStringReadAndWrite() {
+        // give
+        val key = "string-key"
+        val appleSettings = AppleSettings(userDefaults)
+
+        // when
+        appleSettings.putString(key, "abc")
+
+        // then
+        assertEquals(appleSettings.getString(key), "abc")
+    }
+
+}
