71c539e22e59ea35bb266184f89fcb80fd33b6d3
watanavex <y_watanabe@yumemi.co.jp>
2021-03-06 22:44:04 +0900

    Delete unused import

--- data/db/src/iosMain/kotlin/io/github/droidkaigi/feeder/data/DataStoreUserDataStore.kt
+++ data/db/src/iosMain/kotlin/io/github/droidkaigi/feeder/data/DataStoreUserDataStore.kt
@@ -1,7 +1,6 @@
 package io.github.droidkaigi.feeder.data
 
 import com.russhwolf.settings.coroutines.toFlowSettings
-import platform.Foundation.NSUserDefaults
 
 class DataStoreUserDataStore constructor(name: String? = null) : UserDataStore() {
     private val appleSetting = AppleSettings.Factory().create(name)
