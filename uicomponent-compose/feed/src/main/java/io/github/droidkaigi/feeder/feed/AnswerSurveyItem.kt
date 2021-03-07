759a73a900266969ef8fe0f6075ae6022a2bca83
tommykw <destinys.lotus.1108mn@gmail.com>
2021-03-07 11:05:48 +0900

    Show background for AnswerSurveyItem Preview

--- uicomponent-compose/feed/src/main/java/io/github/droidkaigi/feeder/feed/AnswerSurveyItem.kt
+++ uicomponent-compose/feed/src/main/java/io/github/droidkaigi/feeder/feed/AnswerSurveyItem.kt
@@ -20,7 +20,7 @@ import androidx.compose.ui.unit.sp
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.constraintlayout.compose.Dimension
 import io.github.droidkaigi.feeder.core.NetworkImage
-import io.github.droidkaigi.feeder.core.theme.ConferenceAppFeederTheme
+import io.github.droidkaigi.feeder.core.theme.AppThemeWithBackground
 
 @Composable
 fun AnswerSurveyItem(
@@ -98,7 +98,7 @@ fun AnswerSurveyItem(
 @Preview
 @Composable
 fun AnswerSurveyItemPreview() {
-    ConferenceAppFeederTheme {
+    AppThemeWithBackground {
         AnswerSurveyItem(
             surveyImage = "",
             surveyMessage = "アンケートにご協力お願いします",
