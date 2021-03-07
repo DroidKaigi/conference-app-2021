96b749eea4e89281a0aa2e822093fef7b8699542
kagemiku <kagemiku@gmail.com>
2021-03-07 10:51:43 +0900

    Add each screen files

---
+++ ios/DroidKaigi 2021/Features/Feeder/FeederScreen.swift
@@ -0,0 +1,20 @@
+//
+//  FeederScreen.swift
+//  DroidKaigi 2021
+//
+//  Created by kagemiku on 2021/03/07.
+//
+
+import SwiftUI
+
+struct FeederScreen: View {
+    var body: some View {
+        Text("Feeder")
+    }
+}
+
+struct FeederScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        FeederScreen()
+    }
+}
