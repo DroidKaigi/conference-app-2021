b9c396fee6912cf646b5695f367dccbd452d5f67
watanavex <y_watanabe@yumemi.co.jp>
2021-03-07 23:21:42 +0900

    Remove UserDefaults wrapper

--- data/db/src/iosMain/kotlin/io/github/droidkaigi/feeder/data/DataStoreUserDataStore.kt
+++ data/db/src/iosMain/kotlin/io/github/droidkaigi/feeder/data/DataStoreUserDataStore.kt
@@ -1,8 +1,22 @@
 package io.github.droidkaigi.feeder.data
 
+import com.russhwolf.settings.AppleSettings
 import com.russhwolf.settings.coroutines.toFlowSettings
+import platform.Foundation.NSUserDefaults
 
-class DataStoreUserDataStore constructor(name: String? = null) : UserDataStore() {
-    private val appleSetting = AppleSettings.Factory().create(name)
+class DataStoreUserDataStore(name: String? = null) : UserDataStore() {
+
+    private val appleSetting: AppleSettings
+
+    init {
+        val userDefaults = if (name != null) {
+            NSUserDefaults(name)
+        } else {
+            NSUserDefaults.standardUserDefaults
+        }
+
+        appleSetting = AppleSettings(userDefaults)
+    }
+
     override val flowSettings = appleSetting.toFlowSettings()
 }
