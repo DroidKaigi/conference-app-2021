c3a789cdc8011523fb45f3b83a4b0f4daae844cd
atsuya046 <nobuya.oshiro@gmail.com>
2021-03-08 21:43:25 +0900

    remove unnecessary SnackBar

--- uicomponent-compose/feed/src/main/java/io/github/droidkaigi/feeder/feed/FeedScreen.kt
+++ uicomponent-compose/feed/src/main/java/io/github/droidkaigi/feeder/feed/FeedScreen.kt
@@ -97,9 +97,6 @@ fun FeedScreen(
                     )
                 ) {
                     SnackbarResult.ActionPerformed -> {
-                        scaffoldState.snackbarHostState.showSnackbar(
-                            "Sorry, Currently not implemented."
-                        )
                         dispatch(FeedViewModel.Event.ReloadContent)
                     }
                     SnackbarResult.Dismissed -> {
