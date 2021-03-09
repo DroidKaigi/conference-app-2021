85a2a22e41540e31271f3c9fe4da21c94eb107f1
anandwana001 <akshaynandwana001@gmail.com>
2021-03-09 12:50:14 +0530

    added unit test

--- uicomponent-compose/feed/src/androidTest/java/io/github/droidkaigi/feeder/feed/FeedItemTest.kt
+++ uicomponent-compose/feed/src/androidTest/java/io/github/droidkaigi/feeder/feed/FeedItemTest.kt
@@ -2,8 +2,10 @@ package io.github.droidkaigi.feeder.feed
 
 import androidx.activity.ComponentActivity
 import androidx.compose.ui.test.assertHasClickAction
+import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.onNodeWithText
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import io.github.droidkaigi.feeder.core.theme.ConferenceAppFeederTheme
 import io.github.droidkaigi.feeder.fakeFeedContents
@@ -33,5 +35,45 @@ class FeedItemTest {
         }
 
         composeTestRule.onNodeWithTag("FeedItem").assertHasClickAction()
+    }
+
+    @Test
+    fun showFeedItemWithMedia_mediaLabelIsDisplayed() {
+        val feedItem = fakeFeedContents().contents[0].first
+        val mediaText = feedItem.media.text
+
+        composeTestRule.setContent {
+            ConferenceAppFeederTheme {
+                FeedItem(
+                    feedItem = feedItem,
+                    favorited = true,
+                    showMediaLabel = true,
+                    onClick = { },
+                    onFavoriteChange = { }
+                )
+            }
+        }
+
+        composeTestRule.onNodeWithText(mediaText, useUnmergedTree = true).assertIsDisplayed()
+    }
+
+    @Test
+    fun showFeedItemWithoutMedia_mediaLabelDoesNotExist() {
+        val feedItem = fakeFeedContents().contents[0].first
+        val mediaText = feedItem.media.text
+
+        composeTestRule.setContent {
+            ConferenceAppFeederTheme {
+                FeedItem(
+                    feedItem = feedItem,
+                    favorited = true,
+                    showMediaLabel = false,
+                    onClick = { },
+                    onFavoriteChange = { }
+                )
+            }
+        }
+
+        composeTestRule.onNodeWithText(mediaText, useUnmergedTree = true).assertDoesNotExist()
     }
 }
