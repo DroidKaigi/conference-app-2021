fe77dc4921fa3a586c5877af7bb5d1a511089626
t-nonomura <nono.develop@gmail.com>
2021-03-12 17:46:32 +0900

    Get app version name on About this app screen

---
+++ buildSrc/src/main/java/AppVersions.kt
@@ -0,0 +1,10 @@
+package io.github.droidkaigi.feeder
+
+object AppVersions {
+    private const val versionMajor = 1
+    private const val versionMinor = 0
+    private const val versionPatch = 0
+
+    const val versionCode = versionMajor * 10000 + versionMinor * 100 + versionPatch
+    const val versionName = "$versionMajor.$versionMinor.$versionPatch"
+}
--- uicomponent-compose/other/src/main/java/io/github/droidkaigi/feeder/about/AboutThisApp.kt
+++ uicomponent-compose/other/src/main/java/io/github/droidkaigi/feeder/about/AboutThisApp.kt
@@ -19,6 +19,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import io.github.droidkaigi.feeder.core.theme.typography
@@ -38,7 +39,7 @@ fun AboutThisApp() {
         AboutThisAppMenuListComponent(
             onClickPrivacyPolicy = {},
             onClickLicense = {},
-            appVersion = "1.0.0" // Get the app version name.
+            appVersion = stringResource(R.string.app_version_name)
         )
     }
 }
