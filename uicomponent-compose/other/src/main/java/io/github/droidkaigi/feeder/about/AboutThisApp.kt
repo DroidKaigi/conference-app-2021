7db04a9c8cbb52e35a74eb4a0c70e6022ab685fd
KKusumi <kusumi_katsumi@andfactory.co.jp>
2021-03-18 10:41:12 +0900

    Remove unused import.

--- uicomponent-compose/other/src/main/java/io/github/droidkaigi/feeder/about/AboutThisApp.kt
+++ uicomponent-compose/other/src/main/java/io/github/droidkaigi/feeder/about/AboutThisApp.kt
@@ -24,7 +24,6 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.core.content.ContextCompat.startActivity
 import com.google.android.gms.oss.licenses.OssLicensesMenuActivity
 import io.github.droidkaigi.feeder.core.theme.typography
 import io.github.droidkaigi.feeder.other.R
