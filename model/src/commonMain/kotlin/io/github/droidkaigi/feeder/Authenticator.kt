3e2e1d50a297d808952dacff89525d36f9943b43
watanavex <y_watanabe@yumemi.co.jp>
2021-04-16 05:22:46 +0900

    Declare interface of firebase wrapper

---
+++ model/src/commonMain/kotlin/io/github/droidkaigi/feeder/Authenticator.kt
@@ -0,0 +1,12 @@
+package io.github.droidkaigi.feeder
+
+data class User(
+    val idToken: String?
+)
+
+interface Authenticator {
+
+    suspend fun currentUser() : User?
+    suspend fun signInAnonymously() : User?
+
+}
