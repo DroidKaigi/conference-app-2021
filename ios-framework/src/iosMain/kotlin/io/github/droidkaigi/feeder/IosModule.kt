3b04920646860de4fc36e2a18e11148885096f3f
watanavex <y_watanabe@yumemi.co.jp>
2021-04-17 14:18:19 +0900

    Inject a wrapper for Firebase auth

--- ios-framework/src/iosMain/kotlin/io/github/droidkaigi/feeder/IosModule.kt
+++ ios-framework/src/iosMain/kotlin/io/github/droidkaigi/feeder/IosModule.kt
@@ -1,10 +1,21 @@
 package io.github.droidkaigi.feeder
 
+import io.github.droidkaigi.feeder.data.apiModule
+import io.github.droidkaigi.feeder.data.databaseModule
+import io.github.droidkaigi.feeder.data.repositoryModule
 import org.koin.core.KoinApplication
 import org.koin.core.context.startKoin
+import org.koin.dsl.module
 
-fun initKoin(): KoinApplication {
+fun initKoin(authenticator: Authenticator): KoinApplication {
     return startKoin {
-        modules(databaseModule, apiModule, repositoryModule)
+        modules(
+            databaseModule,
+            apiModule,
+            repositoryModule,
+            module {
+                single { authenticator }
+            }
+        )
     }
 }
