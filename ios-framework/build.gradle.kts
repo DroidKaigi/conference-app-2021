3d31a772879653f24aee1d1615b2ae6b0616adc7
watanavex <y_watanabe@yumemi.co.jp>
2021-04-20 17:58:17 +0900

    Fix workaround

--- ios-framework/build.gradle.kts
+++ ios-framework/build.gradle.kts
@@ -66,4 +66,4 @@ val packForXcode by tasks.creating(Sync::class) {
 tasks.getByName("build").dependsOn(packForXcode)
 
 // Workaround for issues where types defined in iOS native code cannot be referenced in Android Studio
-tasks.getByName("preBuild").dependsOn(tasks.getByName("iosMainKlibrary"))
+tasks.getByName("preBuild").dependsOn(tasks.getByName("compileKotlinIos"))
