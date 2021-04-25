ea8a7f9d9c31b9cc4278167d3f7c8b7ac97d5b84
watanavex <y_watanabe@yumemi.co.jp>
2021-04-25 21:53:32 +0900

    Fix dependencies for ios

--- ios-framework/src/iosMain/kotlin/io/github/droidkaigi/feeder/IosModule.kt
+++ ios-framework/src/iosMain/kotlin/io/github/droidkaigi/feeder/IosModule.kt
@@ -3,6 +3,10 @@ package io.github.droidkaigi.feeder
 import io.github.droidkaigi.feeder.data.apiModule
 import io.github.droidkaigi.feeder.data.databaseModule
 import io.github.droidkaigi.feeder.data.repositoryModule
+import io.github.droidkaigi.feeder.repository.platformModule
+import kotlinx.cinterop.ObjCProtocol
+import kotlinx.cinterop.getOriginalKotlinClass
+import org.koin.core.Koin
 import org.koin.core.KoinApplication
 import org.koin.core.context.startKoin
 import org.koin.dsl.module
@@ -13,9 +17,16 @@ fun initKoin(authenticator: Authenticator): KoinApplication {
             databaseModule,
             apiModule,
             repositoryModule,
+            platformModule,
             module {
+                single<ScopeProvider> { MainScopeProvider() }
                 single { authenticator }
             }
         )
     }
+}
+
+fun Koin.get(objCProtocol: ObjCProtocol): Any {
+    val kClazz = getOriginalKotlinClass(objCProtocol)!!
+    return get(kClazz)
 }
---
+++ ios-framework/src/iosMain/kotlin/io/github/droidkaigi/feeder/repository/PlatformModule.kt
@@ -0,0 +1,25 @@
+package io.github.droidkaigi.feeder.repository
+
+import org.koin.dsl.module
+
+val platformModule = module {
+    single<IosContributorRepository> {
+        IosContributorRepositoryImpl(get())
+    }
+
+    single<IosDeviceRepository> {
+        IosDeviceRepositoryImpl(get())
+    }
+
+    single<IosFeedRepository> {
+        IosFeedRepositoryImpl(get())
+    }
+
+    single<IosStaffRepository> {
+        IosStaffRepositoryImpl(get())
+    }
+
+    single<IosThemeRepository> {
+        IosThemeRepositoryImpl(get())
+    }
+}
