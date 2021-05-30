85a28ee89298eb80fcf65d29ad5665eeaea77d8c
ryoya ito <110ryoya@gmail.com>
2021-05-31 00:47:38 +0900

    Use SPM instead of Mint

---
+++ ios/Tools/Package.swift
@@ -0,0 +1,14 @@
+// swift-tools-version:5.3
+
+import PackageDescription
+
+let package = Package(
+    name: "Tools",
+    products: [
+    ],
+    dependencies: [
+        .package(url: "https://github.com/realm/SwiftLint.git", .exact("0.43.1")),
+    ],
+    targets: [
+    ]
+)
--- ios/scripts/lint.sh
+++ ios/scripts/lint.sh
@@ -3,6 +3,19 @@
 SHELL_PATH=`pwd -P`
 cd "$SHELL_PATH/../"
 
-xcrun --sdk macosx mint run swiftlint --fix
-xcrun --sdk macosx mint run swiftlint
+shopt -s expand_aliases
 
+alias package-run='xcrun --sdk macosx swift run --package-path Tools'
+
+CHANGED_FILES=$(git status -s | grep -E '^\s*[^D]+\s+.*\.swift$' | awk -F ' ' '{print $2}')
+
+if [ -z $CHANGED_FILES ]; then
+    echo "No files changed. Skip prebuild script."
+    exit 0
+fi
+
+echo execute SwiftLint
+echo $CHANGED_FILES | while read filename; do
+    package-run swiftlint --fix --path $filename
+    package-run swiftlint --path $filename
+done
