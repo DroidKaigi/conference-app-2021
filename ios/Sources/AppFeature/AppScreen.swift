b5fe7cb0b31e6804c99f4f48bcf28328b8c44a30
ryoya ito <110ryoya@gmail.com>
2021-05-31 12:04:40 +0900

    Implement AppScreen with Tab Cases

--- ios/Sources/AppFeature/AppScreen.swift
+++ ios/Sources/AppFeature/AppScreen.swift
@@ -4,35 +4,56 @@ import HomeFeature
 import MediaFeature
 import SwiftUI
 
+enum AppTab: CaseIterable {
+    case home
+    case media
+    case favorites
+    case about
+
+    var title: String {
+        switch self {
+        case .home:
+            return "Home"
+        case .media:
+            return "Media"
+        case .favorites:
+            return "Favorites"
+        case .about:
+            return "About"
+        }
+    }
+
+    var view: some View {
+        switch self {
+        case .home:
+            return AnyView(HomeScreen())
+        case .media:
+            return AnyView(MediaScreen())
+        case .favorites:
+            return AnyView(FavoritesScreen())
+        case .about:
+            return AnyView(AboutScreen())
+        }
+    }
+}
+
 public struct AppScreen: View {
     @State var selection = 0
 
-    public init() {}
+    public init() {
+    }
 
     public var body: some View {
         TabView(
             selection: $selection,
             content: {
-                HomeScreen()
-                    .tabItem {
-                        Text("Home")
-                    }
-                    .tag(0)
-                MediaScreen()
-                    .tabItem {
-                        Text("Media")
-                    }
-                    .tag(1)
-                FavoritesScreen()
-                    .tabItem {
-                        Text("Favorites")
-                    }
-                    .tag(2)
-                AboutScreen()
-                    .tabItem {
-                        Text("About")
-                    }
-                    .tag(3)
+                ForEach(Array(AppTab.allCases.enumerated()), id: \.offset) { (offset, tab) in
+                    tab.view
+                        .tabItem {
+                            Text(tab.title)
+                        }
+                        .tag(offset)
+                }
             }
         )
     }
