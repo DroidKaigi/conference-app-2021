0d1ce99d965572248bc2e648b8aebbb222005c4c
ryoya ito <110ryoya@gmail.com>
2021-06-02 00:17:42 +0900

    Implement Home questionare

--- ios/Sources/HomeFeature/HomeScreen.swift
+++ ios/Sources/HomeFeature/HomeScreen.swift
@@ -14,9 +14,51 @@ public struct HomeScreen: View {
         ZStack(alignment: .top) {
             NavigationView {
                 ScrollView {
-                    Color(AssetColor.primary.color)
-                        .frame(width: nil, height: 200)
-                }.navigationBarItems(
+                    ZStack(alignment: .top) {
+                        Color(AssetColor.primary.color)
+                            .frame(width: nil, height: 200)
+                        VStack(alignment: .trailing, spacing: 0) {
+                            Text("DroidKaigi 2021 (7/31) D-7")
+                                .foregroundColor(Color(AssetColor.Base.white.color))
+                                .padding(.vertical, 12)
+                                .padding(.horizontal, 8)
+                                .background(Color(AssetColor.primaryDark.color))
+                                .padding(.vertical)
+                            Rectangle()
+                                .frame(width: nil, height: 300)
+                            Divider()
+                                .foregroundColor(Color(AssetColor.Separate.contents.color))
+                            VStack(alignment: .trailing, spacing: 12) {
+                                HStack {
+                                    Image(uiImage: AssetImage.logo.image)
+                                    Text("アンケートにご協力をお願いします")
+                                        .foregroundColor(Color(AssetColor.Base.primary.color))
+                                        .font(.headline)
+                                    Spacer()
+                                }
+                                Button(
+                                    action: {},
+                                    label: {
+                                        Text("回答")
+                                            .foregroundColor(Color(AssetColor.primary.color))
+                                            .padding(.vertical, 8)
+                                            .padding(.horizontal, 32)
+                                            .overlay(
+                                                Rectangle()
+                                                    .stroke(Color(AssetColor.primary.color))
+                                            )
+                                    }
+                                )
+                            }
+                            .padding(.vertical, 16)
+                            Divider()
+                                .foregroundColor(Color(AssetColor.Separate.contents.color))
+                        }
+                        .padding(.horizontal)
+                    }
+                }
+                .navigationBarTitle("", displayMode: .inline)
+                .navigationBarItems(
                     trailing: Image(uiImage: AssetImage.iconSetting.image)
                 )
             }
@@ -34,6 +76,6 @@ public struct HomeScreen_Previews: PreviewProvider {
                 reducer: homeReducer,
                 environment: .init()
             )
-        )
+        ).previewDevice(.init(rawValue: "iPhone 12"))
     }
 }
