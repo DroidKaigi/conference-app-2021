f11d53b7223411e5a51cfe3cab77992c79d08309
Jierong Li <ihsan.lee@gmail.com>
2021-06-06 21:13:48 +0900

    Re-Indent

--- ios/Sources/HomeFeature/HomeScreen.swift
+++ ios/Sources/HomeFeature/HomeScreen.swift
@@ -10,52 +10,52 @@ public struct HomeScreen: View {
     }
 
     public var body: some View {
-            NavigationView {
-                ScrollView {
-                    ZStack(alignment: .top) {
-                        AssetColor.primary.color
-                            .frame(width: nil, height: 200)
-                        WithViewStore(store) { viewStore in
-                            VStack(alignment: .trailing, spacing: 0) {
-                                Text("DroidKaigi 2021 (7/31) D-7")
-                                    .foregroundColor(AssetColor.Base.white.color)
-                                    .padding(.vertical, 12)
-                                    .padding(.horizontal, 8)
-                                    .background(AssetColor.primaryDark.color)
-                                    .padding(.vertical)
-                                // TODO: Replace with card(large)
-                                Rectangle()
-                                    .frame(width: nil, height: 300)
-                                Divider()
-                                    .foregroundColor(AssetColor.Separate.contents.color)
-                                QuestionnaireView(tapAnswerAction: {
-                                    viewStore.send(.answerQuestionnaire)
-                                })
-                                Divider()
-                                    .foregroundColor(AssetColor.Separate.contents.color)
-                                ForEach(viewStore.contents, id: \.self) { content in
-                                    // TODO: Replace with List Item
-                                    Text(content)
-                                        .foregroundColor(AssetColor.Base.primary.color)
-                                }
+        NavigationView {
+            ScrollView {
+                ZStack(alignment: .top) {
+                    AssetColor.primary.color
+                        .frame(width: nil, height: 200)
+                    WithViewStore(store) { viewStore in
+                        VStack(alignment: .trailing, spacing: 0) {
+                            Text("DroidKaigi 2021 (7/31) D-7")
+                                .foregroundColor(AssetColor.Base.white.color)
+                                .padding(.vertical, 12)
+                                .padding(.horizontal, 8)
+                                .background(AssetColor.primaryDark.color)
+                                .padding(.vertical)
+                            // TODO: Replace with card(large)
+                            Rectangle()
+                                .frame(width: nil, height: 300)
+                            Divider()
+                                .foregroundColor(AssetColor.Separate.contents.color)
+                            QuestionnaireView(tapAnswerAction: {
+                                viewStore.send(.answerQuestionnaire)
+                            })
+                            Divider()
+                                .foregroundColor(AssetColor.Separate.contents.color)
+                            ForEach(viewStore.contents, id: \.self) { content in
+                                // TODO: Replace with List Item
+                                Text(content)
+                                    .foregroundColor(AssetColor.Base.primary.color)
                             }
-                            .padding(.horizontal)
                         }
+                        .padding(.horizontal)
                     }
                 }
-                .background(AssetColor.Background.primary.color)
-                .navigationBarTitleDisplayMode(.inline)
-                .toolbar {
-                    ToolbarItem(placement: .principal) {
-                        AssetImage.logoTitle.image
-                    }
-                }
-                .navigationBarItems(
-                    trailing: AssetImage.iconSetting.image
-                        .renderingMode(.template)
-                        .foregroundColor(AssetColor.Base.primary.color)
-                )
             }
+            .background(AssetColor.Background.primary.color)
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .principal) {
+                    AssetImage.logoTitle.image
+                }
+            }
+            .navigationBarItems(
+                trailing: AssetImage.iconSetting.image
+                    .renderingMode(.template)
+                    .foregroundColor(AssetColor.Base.primary.color)
+            )
+        }
     }
 }
 
