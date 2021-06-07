11ddad9591f479c0d8fee500307e927f57ccf81a
shun-sasano <jumpjudo3303@gmail.com>
2021-06-07 09:53:55 +0900

    add dummydata to init

--- ios/Sources/AboutFeature/AboutFeature.swift
+++ ios/Sources/AboutFeature/AboutFeature.swift
@@ -4,10 +4,12 @@ public struct AboutState: Equatable {
     public var staffs: [Staff]
     public var contributors: [Contributor]
     public var selectedType: SelectedType
-    
+
     public init(staffs: [Staff] = [], contributors: [Contributor] = [], selectedType: SelectedType = .staff) {
-        self.staffs = staffs
-        self.contributors = contributors
+        // TODO: Replace with real data.
+        self.staffs = dummyStaffs
+        // TODO: Replace with real data.
+        self.contributors = dummyContributors
         self.selectedType = selectedType
     }
 }
@@ -20,11 +22,10 @@ public struct AboutEnvironment {
     public init() {}
 }
 
-public let aboutReducer = Reducer<AboutState, AboutAction, AboutEnvironment> { state, action, environment in
+public let aboutReducer = Reducer<AboutState, AboutAction, AboutEnvironment> { state, action, _ in
     switch action {
     case .selectedPicker(let selectedType):
         state.selectedType = selectedType
         return .none
     }
 }
-
