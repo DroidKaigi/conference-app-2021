a7e7be6e00f7987ce8425d0870e591a499c20410
takumaosada <judgment9ttk069@gmail.com>
2021-06-09 00:48:36 +0900

    create placeholder

--- ios/Sources/Component/ImageView/ImageView.swift
+++ ios/Sources/Component/ImageView/ImageView.swift
@@ -2,23 +2,43 @@ import SwiftUI
 import Styleguide
 
 public struct ImageView: View {
+    enum placeholderType {
+        case noImage
+        case noImagePodcast
+    }
     private let imageURL: URL?
+    private let placeholderType: placeholderType
 
     public init(
         imageURL: URL?
+        placeholderType: placeholderType = .noImage
     ) {
         self.imageURL = imageURL
+        self.placeholder = placeholder
     }
 
     public var body: some View {
-        // TODO: add placeholder image
-        Image("")
+        // TODO: load image from url
+        let image = imageURL != nil ? Image("") : placeholder.image
+
+        image
             .resizable()
-            .background(AssetColor.Base.secondary.color)
+            .background(AssetColor.Background.secondary.color)
             .overlay(
                 RoundedRectangle(cornerRadius: 2)
                     .stroke(AssetColor.Separate.image.color, lineWidth: 1)
             )
+    }
+}
+
+extension ImageView.placeholderType {
+    private var image: Image {
+        switch self {
+        case .noImage:
+            return AssetImage.noImage.image
+        case .noImagePodcast:
+            return AssetImage.noImagePodcast.image
+        }
     }
 }
 
