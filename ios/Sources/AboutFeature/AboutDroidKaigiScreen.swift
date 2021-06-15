8d7aa9c42b3a1a2ae32a22444f0a09efbef7e77c
Tech-Nayuta <ketanayutawork@gmail.com>
2021-06-15 22:14:21 +0900

    Make AboutDroidKaigiScreen

---
+++ ios/Sources/AboutFeature/AboutDroidKaigiScreen.swift
@@ -0,0 +1,87 @@
+import SwiftUI
+import Styleguide
+
+public struct AboutDroidKaigiScreen: View {
+    @Environment(\.presentationMode) var presentationMode
+
+    public init() {
+        UITableView.appearance().backgroundColor = .clear
+        UINavigationBar.appearance().backgroundColor = .clear
+        UINavigationBar.appearance().barTintColor = .clear
+    }
+
+    public var body: some View {
+        NavigationView {
+            VStack {
+                VStack {
+                    Text("What is").font(.system(size: 22))
+                    AssetImage.logoTitle.image
+                        .frame(width: 261.81, height: 41.06, alignment: .center)
+                    Text("DroidKaigiはエンジニアが主役のAndroid\nカンファレンスです。Android技術情報\nの共有とコミュニケーションを目的\nに、イベントを開催しています。")
+                        .multilineTextAlignment(.center)
+                        .font(.system(size: 17))
+                        .opacity(0.7)
+                }
+                .padding(.horizontal, 32)
+
+                List {
+                    Button(action: {}, label: {
+                        HStack() {
+                            Text("行動規範")
+                            Spacer()
+                            Image(systemName: "book")
+                        }
+                    })
+                    .listRowBackground(AssetColor.Background.contents.color)
+                    .foregroundColor(AssetColor.Base.secondary.color)
+
+                    Button(action: {}, label: {
+                        HStack() {
+                            Text("オープンソースライセース")
+                            Spacer()
+                            Image(systemName: "star")
+                        }
+                    })
+                    .listRowBackground(AssetColor.Background.contents.color)
+                    .foregroundColor(AssetColor.Base.secondary.color)
+
+                    Button(action: {}, label: {
+                        HStack() {
+                            Text("プライバシーポリシー")
+                            Spacer()
+                            Image(systemName: "magnifyingglass")
+                        }
+                    })
+                    .listRowBackground(AssetColor.Background.contents.color)
+                    .foregroundColor(AssetColor.Base.secondary.color)
+                }
+                .listStyle(InsetGroupedListStyle())
+
+            }
+            .background(AssetColor.Background.secondary.color.ignoresSafeArea())
+            .navigationBarItems(
+                trailing: Button(action: {
+                    presentationMode.wrappedValue.dismiss()
+                }, label: {
+                    AssetImage.iconClose.image
+                        .renderingMode(.template)
+                        .foregroundColor(AssetColor.Base.primary.color)
+                })
+            )
+        }
+    }
+}
+
+struct AboutDroidKaigiScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            AboutDroidKaigiScreen()
+                .previewDevice(.init(rawValue: "iPhone 12"))
+                .environment(\.colorScheme, .dark)
+
+            AboutDroidKaigiScreen()
+                .previewDevice(.init(rawValue: "iPhone 12"))
+                .environment(\.colorScheme, .light)
+        }
+    }
+}
