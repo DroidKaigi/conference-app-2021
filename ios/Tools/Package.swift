5937cfe610f376a545347a793acf48a12dd919ba
woxtu <woxtup@gmail.com>
2021-06-16 22:13:26 +0900

    Introduce LicensePlist

--- ios/Tools/Package.swift
+++ ios/Tools/Package.swift
@@ -7,6 +7,7 @@ let package = Package(
     products: [
     ],
     dependencies: [
+        .package(url: "https://github.com/mono0926/LicensePlist.git", .exact("3.13.0")),
         .package(url: "https://github.com/SwiftGen/SwiftGen.git", .exact("6.4.0")),
         .package(url: "https://github.com/realm/SwiftLint.git", .exact("0.43.1")),
     ],
