55a6936726ad1d9d4c33980936bafb1de535f681
takumaosada <judgment9ttk069@gmail.com>
2021-06-20 10:58:48 +0900

    show log prettier

--- ios/Tools/Package.swift
+++ ios/Tools/Package.swift
@@ -10,6 +10,7 @@ let package = Package(
         .package(url: "https://github.com/mono0926/LicensePlist.git", .exact("3.13.0")),
         .package(url: "https://github.com/SwiftGen/SwiftGen.git", .exact("6.4.0")),
         .package(url: "https://github.com/realm/SwiftLint.git", .exact("0.43.1")),
+        .package(url: "https://github.com/thii/xcbeautify.git", .exact("0.9.1")),
     ],
     targets: [
     ]
---
+++ ios/scripts/xcodebuild.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+TYPE=${1}
+SCHEME=${2}
+CONFIGURATION=${3}
+
+WORKSPACE=DroidKaigi2021.xcworkspace
+PLATFORM_IOS="iOS Simulator,name=iPhone 12 Pro,OS=14.4"
+
+echo "⚙️  Building $SCHEME..."
+set -o pipefail && xcodebuild $TYPE \
+-workspace $WORKSPACE \
+-scheme $SCHEME \
+-configuration $CONFIGURATION \
+-destination platform="$PLATFORM_IOS" | ./scripts/swift.sh xcbeautify --quiet
