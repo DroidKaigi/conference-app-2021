4f450221ed83847e6945e84a0f6234c21416be1f
ryoya ito <110ryoya@gmail.com>
2021-06-21 23:29:39 +0900

    Add doc comment and record argument

--- ios/Tests/TestUtils/PreviewSnapshot.swift
+++ ios/Tests/TestUtils/PreviewSnapshot.swift
@@ -2,8 +2,17 @@ import SnapshotTesting
 import SwiftUI
 import XCTest
 
+/// Asserts that a given preview contents match on disk.
+///
+/// - Parameters:
+///   - target: Target preview type
+///   - recording: Whether or not to record a new reference. If component that was already recorded, make this property to `true` and re-test.
+///   - file: The file in which failure occurred. Defaults to the file name of the test case in which this function was called.
+///   - testName: The name of the test in which failure occurred. Defaults to the function name of the test case in which this function was called.
+///   - line: The line number on which failure occurred. Defaults to the line number on which this function was called.
 public func assertPreviewSnapshot<T: PreviewProvider>(
-    _ tareget: T.Type,
+    _ target: T.Type,
+    record recording: Bool = false,
     file: StaticString = #file,
     testName: String = #function,
     line: UInt = #line
@@ -12,6 +21,7 @@ public func assertPreviewSnapshot<T: PreviewProvider>(
         assertSnapshot(
             matching: preview.content,
             as: .image,
+            record: recording,
             file: file,
             testName: testName,
             line: line
