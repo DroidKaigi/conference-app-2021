1dcba6640f7e389343bdf13b9de9b1b2da9af661
ryoya ito <110ryoya@gmail.com>
2021-06-30 12:12:57 +0900

    Fix repository

--- ios/Sources/Repository/FeedRepository.swift
+++ ios/Sources/Repository/FeedRepository.swift
@@ -19,18 +19,34 @@ public struct FeedRepository: FeedRepositoryProtocol, KMMRepositoryProtocol {
         self.repository = container.get(type: RepositoryType.self)
     }
 
-    public func feedContents() -> AnyPublisher<[FeedContent], KotlinError> {
-        Future<FeedContents, KotlinError> { promise in
-            repository.feedContents()
-                .subscribe(scope: scopeProvider.scope) {
-                    promise(.success($0))
-                } onComplete: {
+    private func refresh() -> AnyPublisher<Void, KotlinError> {
+        Future<Void, KotlinError> { promise in
+            repository.refresh()
+                .subscribe(scope: scopeProvider.scope) { _ in
+                    promise(.success(()))
                 } onFailure: {
                     promise(.failure(KotlinError.fetchFailed($0.description())))
                 }
         }
-        .map([FeedContent].init(from:))
         .eraseToAnyPublisher()
+    }
+
+    public func feedContents() -> AnyPublisher<[FeedContent], KotlinError> {
+        refresh()
+            .flatMap {
+                Future<FeedContents, KotlinError> { promise in
+                    repository.feedContents()
+                        .subscribe(scope: scopeProvider.scope) {
+                            promise(.success($0))
+                        } onComplete: {
+                        } onFailure: {
+                            promise(.failure(KotlinError.fetchFailed($0.description())))
+                        }
+                }
+                .map([FeedContent].init(from:))
+                .eraseToAnyPublisher()
+            }
+            .eraseToAnyPublisher()
     }
 
     public func addFavorite(feedItem: AnyFeedItem) -> AnyPublisher<Void, KotlinError> {
