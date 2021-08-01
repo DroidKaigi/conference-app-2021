94bd1ccfe62e676044ea8bf6a2bf65f820ae9bdc
takahirom <takam.dev@gmail.com>
2021-08-01 16:10:37 +0900

    Update Jetpack libraries versions related to Compose

--- buildSrc/src/main/java/Dep.kt
+++ buildSrc/src/main/java/Dep.kt
@@ -7,10 +7,10 @@ object Versions {
 
 object Dep {
     object Compose {
-        const val activity = "androidx.activity:activity-compose:1.3.0-alpha08"
+        const val activity = "androidx.activity:activity-compose:1.3.0"
         const val constraintLayout =
-            "androidx.constraintlayout:constraintlayout-compose:1.0.0-alpha07"
-        const val navigation = "androidx.navigation:navigation-compose:2.4.0-alpha01"
+            "androidx.constraintlayout:constraintlayout-compose:1.0.0-beta02"
+        const val navigation = "androidx.navigation:navigation-compose:2.4.0-alpha05"
         const val livedata = "androidx.compose.runtime:runtime-livedata:${Versions.compose}"
         const val runtime = "androidx.compose.runtime:runtime:${Versions.compose}"
         const val foundation = "androidx.compose.foundation:foundation:${Versions.compose}"
