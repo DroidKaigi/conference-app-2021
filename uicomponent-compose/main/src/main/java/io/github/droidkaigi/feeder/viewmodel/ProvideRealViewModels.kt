26d8030f848f85b711c6011d6a2278522731add1
takahirom <takam.dev@gmail.com>
2021-09-20 09:05:21 +0900

    Use fake ViewModel

--- uicomponent-compose/main/src/main/java/io/github/droidkaigi/feeder/viewmodel/ProvideRealViewModels.kt
+++ uicomponent-compose/main/src/main/java/io/github/droidkaigi/feeder/viewmodel/ProvideRealViewModels.kt
@@ -10,6 +10,7 @@ import io.github.droidkaigi.feeder.feed.provideFmPlayerViewModelFactory
 import io.github.droidkaigi.feeder.provideAppViewModelFactory
 import io.github.droidkaigi.feeder.setting.provideSettingViewModelFactory
 import io.github.droidkaigi.feeder.staff.provideStaffViewModelFactory
+import io.github.droidkaigi.feeder.timetable2021.fakeTimetableViewModel
 import io.github.droidkaigi.feeder.timetable2021.provideTimetableViewModelFactory
 
 @Composable
@@ -17,7 +18,7 @@ fun ProvideViewModels(content: @Composable () -> Unit) {
     CompositionLocalProvider(
         provideAppViewModelFactory { hiltViewModel<RealAppViewModel>() },
         provideFeedViewModelFactory { hiltViewModel<RealFeedViewModel>() },
-        provideTimetableViewModelFactory { hiltViewModel<RealTimetableViewModel>() },
+        provideTimetableViewModelFactory { fakeTimetableViewModel() },
         provideSettingViewModelFactory { hiltViewModel<RealSettingViewModel>() },
         provideStaffViewModelFactory { (hiltViewModel<RealStaffViewModel>()) },
         provideContributorViewModelFactory { fakeContributorViewModel() },
