8e762a376b09502dfb864baee20d1298cebf6f9a
417-72KI <417.72ki@gmail.com>
2021-09-21 02:23:35 +0900

    rename properties to fit real APIs

--- ios/Sources/Model/Staff.swift
+++ ios/Sources/Model/Staff.swift
@@ -2,27 +2,27 @@ import DroidKaigiMPP
 
 public struct Staff: Equatable, Identifiable {
     public var id: Int
-    public var imageURLString: String
-    public var name: String
-    public var urlString: String
+    public var iconURLString: String
+    public var username: String
+    public var profileURLString: String
 
     public init(
         id: Int,
-        imageURLString: String,
-        name: String,
-        urlString: String
+        iconURLString: String,
+        username: String,
+        profileURLString: String
     ) {
         self.id = id
-        self.imageURLString = imageURLString
-        self.name = name
-        self.urlString = urlString
+        self.iconURLString = iconURLString
+        self.username = username
+        self.profileURLString = profileURLString
     }
 
     public init(from model: DroidKaigiMPP.Staff) {
         self.id = Int(model.id)
-        self.imageURLString = model.iconUrl
-        self.name = model.username
-        self.urlString = model.profileUrl
+        self.iconURLString = model.iconUrl
+        self.username = model.username
+        self.profileURLString = model.profileUrl
     }
 }
 
@@ -30,15 +30,15 @@ public struct Staff: Equatable, Identifiable {
 public extension Staff {
     static func mock(
         id: Int = UUID().uuidString.hash,
-        imageURLString: String = "https://example.com",
-        name: String = "dummy name",
-        urlString: String = "https://github.com"
+        iconURLString: String = "https://example.com",
+        username: String = "dummy username",
+        profileURLString: String = "https://github.com"
     ) -> Self {
         .init(
             id: id,
-            imageURLString: imageURLString,
-            name: name,
-            urlString: urlString
+            iconURLString: iconURLString,
+            username: username,
+            profileURLString: profileURLString
         )
     }
 }
