838d9844bc1c8fb7c682be3b23b3d18a331dbe51
417-72KI <417.72ki@gmail.com>
2021-09-21 03:38:51 +0900

    Create Dangerfile.swift draft

---
+++ ios/Dangerfile.swift
@@ -0,0 +1,18 @@
+import Danger
+
+func run(_ danger: DangerDSL) {
+    // Run SwiftLint
+    SwiftLint.lint(inline: true)
+
+    if let github = danger.github {
+        let pr = github.pullRequest
+
+        // Make it more obvious that a PR is a work in progress and shouldn't be merged yet
+        if pr.title.lowercased().contains("[wip]") {
+            danger.warn("PR is classed as Work in Progress")
+        }
+    }
+}
+
+// MARK: -
+run(Danger())
