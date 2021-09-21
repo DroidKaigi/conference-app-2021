fd2cdeb8f4660debc1e509168654d212eb01b840
Haruhiko Takada <haruhiko.takada@gmail.com>
2021-09-22 00:24:11 +0900

    Add banner image to drawer content

--- uicomponent-compose/main/src/main/java/io/github/droidkaigi/feeder/DrawerContent.kt
+++ uicomponent-compose/main/src/main/java/io/github/droidkaigi/feeder/DrawerContent.kt
@@ -24,6 +24,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -199,6 +200,19 @@ fun DrawerContent(
                     }
                 }
             }
+        }
+        Spacer(modifier = Modifier.height(12.dp))
+        Divider()
+        Row(
+            horizontalArrangement = Arrangement.Center,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 16.dp)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.banner_droidkaigi_2021),
+                contentDescription = stringResource(id = R.string.banner_droidkaigi_2021)
+            )
         }
     }
 }
