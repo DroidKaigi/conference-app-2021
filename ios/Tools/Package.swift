5ce3316d2b8ff4ec49e9137805675c9575c020f0
Takuma Osada <judgment9ttk069@gmail.com>
2021-09-25 07:53:22 +0900

    update SPM dependencies

--- ios/Tools/Package.swift
+++ ios/Tools/Package.swift
@@ -7,9 +7,9 @@ let package = Package(
     products: [
     ],
     dependencies: [
-        .package(url: "https://github.com/mono0926/LicensePlist.git", .exact("3.13.0")),
+        .package(url: "https://github.com/mono0926/LicensePlist.git", .exact("3.14.2")),
         .package(url: "https://github.com/SwiftGen/SwiftGen.git", .exact("6.4.0")),
-        .package(url: "https://github.com/realm/SwiftLint.git", .exact("0.43.1")),
+        .package(url: "https://github.com/realm/SwiftLint.git", .exact("0.44.0")),
         .package(url: "https://github.com/thii/xcbeautify.git", .exact("0.9.1")),
     ],
     targets: [
