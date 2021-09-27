d26b67ad11694bd51539a8eb027a657057df9ca7
ryoya ito <110ryoya@gmail.com>
2021-09-28 02:01:17 +0900

    Remove unnecessary VStack

--- ios/Sources/AboutFeature/AboutScreen.swift
+++ ios/Sources/AboutFeature/AboutScreen.swift
@@ -31,56 +31,54 @@ public struct AboutScreen: View {
             NavigationView {
                 WithViewStore(store) { viewStore in
                     ZStack {
-                        VStack {
-                            ScrollView(.vertical) {
-                                switch viewStore.selectedType {
-                                case .staff:
-                                    LazyVStack(alignment: .leading, spacing: 24) {
-                                        ForEach(viewStore.staffs) { staff in
-                                            StaffCell(staff: staff) { staff in
-                                                viewStore.send(.tapStaff(staff))
-                                            }
+                        ScrollView(.vertical) {
+                            switch viewStore.selectedType {
+                            case .staff:
+                                LazyVStack(alignment: .leading, spacing: 24) {
+                                    ForEach(viewStore.staffs) { staff in
+                                        StaffCell(staff: staff) { staff in
+                                            viewStore.send(.tapStaff(staff))
                                         }
                                     }
-                                    .padding(.top, 20)
-                                case .contributor:
-                                    LazyVGrid(
-                                        columns: Array(repeating: .init(), count: 3),
-                                        spacing: 40
-                                    ) {
-                                        ForEach(viewStore.contributors) { contributor in
-                                            ContributorCell(contributor: contributor) { contributor in
-                                                viewStore.send(.tapContributor(contributor))
-                                            }
-                                        }
-                                    }
-                                    .listStyle(PlainListStyle())
-                                    .padding(.top, 20)
                                 }
-                            }
-                            .toolbar {
-                                ToolbarItem(placement: .principal) {
-                                    Picker(
-                                        "",
-                                        selection:
-                                            viewStore.binding(
-                                                get: { $0.selectedType },
-                                                send: { .selectedPicker($0) }
-                                            )
-                                    ) {
-                                        ForEach(SelectedType.allCases, id: \.self) { (type) in
-                                            Text(type.title).tag(type)
+                                .padding(.top, 20)
+                            case .contributor:
+                                LazyVGrid(
+                                    columns: Array(repeating: .init(), count: 3),
+                                    spacing: 40
+                                ) {
+                                    ForEach(viewStore.contributors) { contributor in
+                                        ContributorCell(contributor: contributor) { contributor in
+                                            viewStore.send(.tapContributor(contributor))
                                         }
                                     }
-                                    .frame(width: geometry.size.width - 32, height: nil, alignment: .center)
-                                    .pickerStyle(SegmentedPickerStyle())
                                 }
+                                .listStyle(PlainListStyle())
+                                .padding(.top, 20)
                             }
-                            .background(AssetColor.Background.primary.color.ignoresSafeArea())
-                            .navigationBarTitleDisplayMode(.inline)
-                            .onAppear {
-                                viewStore.send(.refresh)
+                        }
+                        .toolbar {
+                            ToolbarItem(placement: .principal) {
+                                Picker(
+                                    "",
+                                    selection:
+                                        viewStore.binding(
+                                            get: { $0.selectedType },
+                                            send: { .selectedPicker($0) }
+                                        )
+                                ) {
+                                    ForEach(SelectedType.allCases, id: \.self) { (type) in
+                                        Text(type.title).tag(type)
+                                    }
+                                }
+                                .frame(width: geometry.size.width - 32, height: nil, alignment: .center)
+                                .pickerStyle(SegmentedPickerStyle())
                             }
+                        }
+                        .background(AssetColor.Background.primary.color.ignoresSafeArea())
+                        .navigationBarTitleDisplayMode(.inline)
+                        .onAppear {
+                            viewStore.send(.refresh)
                         }
                         banner {
                             viewStore.send(.tapBanner)
