ba49e94c6c0502e116204cdccd511e36c8abd48b
hironobu.iga <hironobu.iga.developer@gmail.com>
2021-09-29 01:55:36 +0900

    Change AboutFeature methods

--- ios/Sources/AboutFeature/AboutFeature.swift
+++ ios/Sources/AboutFeature/AboutFeature.swift
@@ -3,42 +3,20 @@ import ComposableArchitecture
 import Model
 import Repository
 
-public struct AboutState: Equatable {
-    public var staffs: [Staff]
-    public var contributors: [Contributor]
-    public var selectedType: SelectedType
-    public var isSheetPresented: SheetType?
+public enum AboutState: Equatable {
+    case needToInitialize
+    case initialized(AboutLoadedState)
+    case errorOccurred
 
-    public var isShowingSheet: Bool {
-        isSheetPresented != nil
-    }
-
-    public enum SheetType: Equatable {
-        case url(URL)
-        case aboutDroidKaigi
-    }
-
-    public init(
-        staffs: [Staff] = [],
-        contributors: [Contributor] = [],
-        selectedType: SelectedType = .staff,
-        isSheetPresented: SheetType? = nil
-    ) {
-        self.staffs = staffs
-        self.contributors = contributors
-        self.selectedType = selectedType
-        self.isSheetPresented = isSheetPresented
+    public init() {
+        self = .needToInitialize
     }
 }
 
 public enum AboutAction {
     case refresh
     case refreshResponse(Result<([Contributor], [Staff]), KotlinError>)
-    case selectedPicker(SelectedType)
-    case tapStaff(Staff)
-    case tapContributor(Contributor)
-    case tapBanner
-    case hideSheet
+    case loaded(AboutLoadedAction)
 }
 
 public struct AboutEnvironment {
@@ -54,37 +32,36 @@ public struct AboutEnvironment {
     }
 }
 
-public let aboutReducer = Reducer<AboutState, AboutAction, AboutEnvironment> { state, action, environment in
-    switch action {
-    case .refresh:
-        return Publishers.CombineLatest(
-            environment.contributorRepository.contributorContents(),
-            environment.staffRepository.staffContents()
-        )
-        .catchToEffect()
-        .map(AboutAction.refreshResponse)
-    case .refreshResponse(.success((let contributors, let staffs))):
-        state.contributors = contributors
-        state.staffs = staffs
-        return .none
-    case .refreshResponse(.failure):
-        return .none
-    case .selectedPicker(let selectedType):
-        state.selectedType = selectedType
-        return .none
-    case let .tapStaff(staff):
-        guard let url = URL(string: staff.profileURLString) else { return .none }
-        state.isSheetPresented = .url(url)
-        return .none
-    case let .tapContributor(contributor):
-        guard let url = URL(string: contributor.urlString) else { return .none }
-        state.isSheetPresented = .url(url)
-        return .none
-    case .tapBanner:
-        state.isSheetPresented = .aboutDroidKaigi
-        return .none
-    case .hideSheet:
-        state.isSheetPresented = nil
-        return .none
+public let aboutReducer = Reducer<AboutState, AboutAction, AboutEnvironment>.combine(
+    aboutLoadedReducer.pullback(
+        state: /AboutState.initialized,
+        action: /AboutAction.loaded,
+        environment: { _ in
+            .init()
+        }
+    ),
+    .init { state, action, environment in
+        switch action {
+        case .refresh:
+            return Publishers.CombineLatest(
+                environment.contributorRepository.contributorContents(),
+                environment.staffRepository.staffContents()
+            )
+            .catchToEffect()
+            .map(AboutAction.refreshResponse)
+        case .refreshResponse(.success((let contributors, let staffs))):
+            state = .initialized(
+                AboutLoadedState(
+                    staffs: staffs,
+                    contributors: contributors
+                )
+            )
+            return .none
+        case .refreshResponse(.failure):
+            state = .errorOccurred
+            return .none
+        case .loaded:
+            return .none
+        }
     }
-}
+)
