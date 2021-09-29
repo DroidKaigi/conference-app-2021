20de50f20f78b14a1540eadd8500a4c637458bfd
hironobu.iga <hironobu.iga.developer@gmail.com>
2021-09-30 02:02:05 +0900

    Add TimeTableScreen preview cases

--- ios/Sources/TimetableFeature/List/TimetableScreen.swift
+++ ios/Sources/TimetableFeature/List/TimetableScreen.swift
@@ -62,18 +62,63 @@ private extension TimetableAction {
 }
 
 #if DEBUG
+import Model
 import Repository
 
 public struct TimetableScreen_Previews: PreviewProvider {
     public static var previews: some View {
+        let calendar = Calendar.init(identifier: .japanese)
+        let sessionMocks = (0...5).map { num -> AnyTimetableItem in
+            let dateComponents = DateComponents(
+                year: 2021, month: 10,day: 19, hour: 11 + num, minute: 00
+            )
+            return AnyTimetableItem.sessionMock(
+                startsAt: calendar.date(from: dateComponents)!
+            )
+        }
+        
+
+        let timetableItemMocks: [AnyTimetableItem] = [
+            .sessionMock(), .sessionMock(), .specialMock(), .specialMock()
+        ]
+        
+        ForEach(ColorScheme.allCases, id: \.self) { colorScheme in
+            // needToInitialize
+            timeTableScreen(state: .needToInitialize)
+                .previewDevice(PreviewDevice(rawValue: "iPhone 12"))
+                .environment(\.colorScheme, colorScheme)
+
+            // errorOccurred
+            timeTableScreen(state: .errorOccurred)
+                .previewDevice(PreviewDevice(rawValue: "iPhone 12"))
+                .environment(\.colorScheme, colorScheme)
+
+            // initialized
+            timeTableScreen(state: .initialized(timeTableLoadedState(items: [])))
+                .previewDevice(PreviewDevice(rawValue: "iPhone 12"))
+                .environment(\.colorScheme, colorScheme)
+            
+            timeTableScreen(state: .initialized(timeTableLoadedState(items: sessionMocks)))
+                .previewDevice(PreviewDevice(rawValue: "iPhone 12"))
+                .environment(\.colorScheme, colorScheme)
+        }
+    }
+    
+    private static func timeTableScreen(state: TimetableState) -> some View {
         TimetableScreen(
             store: Store<TimetableState, TimetableAction>(
-                initialState: .initialized(.init()),
-                reducer: timetableReducer,
+                initialState: state,
+                reducer: .empty,
                 environment: TimetableEnvironment(
                     timetableRepository: TimetableRepositoryMock()
                 )
             )
+        )
+    }
+    
+    private static func timeTableLoadedState(items: [AnyTimetableItem]) -> TimetableLoadedState {
+        TimetableLoadedState(
+            timetableItems: items
         )
     }
 }
