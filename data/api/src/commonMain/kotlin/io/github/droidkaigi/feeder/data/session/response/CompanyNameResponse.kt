375c181636115403eb1262e090cebdbc11348984
Koji Wakamiya <koji.wakamiya@gmail.com>
2021-10-09 09:08:19 +0900

    Remove unused classes

--- data/api/src/commonMain/kotlin/io/github/droidkaigi/feeder/data/session/response/CompanyNameResponse.kt
+++
@@ -1,9 +0,0 @@
-package io.github.droidkaigi.feeder.data.session.response
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-internal data class CompanyNameResponse(
-    val ja: String,
-    val en: String
-)
--- data/api/src/commonMain/kotlin/io/github/droidkaigi/feeder/data/session/response/TranslatedNameImpl.kt
+++
@@ -1,9 +0,0 @@
-package io.github.droidkaigi.feeder.data.session.response
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-internal data class TranslatedName(
-    val ja: String,
-    val en: String
-)
