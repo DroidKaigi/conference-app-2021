c4742df3b640944dd5fff190d18393104b22ec9a
ryoya ito <110ryoya@gmail.com>
2021-10-17 17:00:54 +0900

    Fix old project name

--- ios/scripts/xcodebuild.sh
+++ ios/scripts/xcodebuild.sh
@@ -5,7 +5,7 @@ SCHEME=${2}
 CONFIGURATION=${3}
 
 SWIFT_RUN="swift run -c release --package-path Tools"
-WORKSPACE=DroidKaigi2021.xcworkspace
+WORKSPACE=DroidKaigiFeeder.xcworkspace
 PLATFORM_IOS="iOS Simulator,name=iPhone 12 Pro,OS=15.0"
 
 echo "⚙️  Building $SCHEME..."
