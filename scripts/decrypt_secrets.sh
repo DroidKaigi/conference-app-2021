07b35be66eb06902f1163b321b50a5c6275c95e1
takahirom <takam.dev@gmail.com>
2021-10-18 20:43:16 +0900

    Fix gitignore

--- scripts/decrypt_secrets.sh
+++ scripts/decrypt_secrets.sh
@@ -3,7 +3,7 @@
 # https://docs.github.com/ja/actions/security-guides/encrypted-secrets
 # Please add decrypted files to .gitignore
 gpg --quiet --batch --yes --decrypt --passphrase="$SECRET_PASSPHRASE" \
-  --output "ios/DroidKaigi 2021/DroidKaigi 2021/GoogleService-Info-Production-Release.plist" "ios/DroidKaigi 2021/DroidKaigi 2021/GoogleService-Info-Production-Release.plist.gpg"
+  --output "ios/DroidKaigi Feeder/DroidKaigi Feeder/GoogleService-Info-Production-Release.plist" "ios/DroidKaigi Feeder/DroidKaigi Feeder/GoogleService-Info-Production-Release.plist.gpg"
 gpg --quiet --batch --yes --decrypt --passphrase="$SECRET_PASSPHRASE" \
   --output "android/src/release/google-services.json" "android/src/release/google-services.json.gpg"
 gpg --quiet --batch --yes --decrypt --passphrase="$SECRET_PASSPHRASE" \
