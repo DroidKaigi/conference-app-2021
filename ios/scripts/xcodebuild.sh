d3d5a59846a79580ed94fe3c5a3d934b100eba87
ryoya ito <110ryoya@gmail.com>
2021-10-24 01:23:19 +0900

    Set -resultBundlePath

--- ios/scripts/xcodebuild.sh
+++ ios/scripts/xcodebuild.sh
@@ -7,10 +7,12 @@ CONFIGURATION=${3}
 SWIFT_RUN="swift run -c release --package-path Tools"
 WORKSPACE=DroidKaigiFeeder.xcworkspace
 PLATFORM_IOS="iOS Simulator,name=iPhone 12 Pro,OS=15.0"
+RESULT_BUNDLE_PATH="BuildResults-$SCHEME-$CONFIGURATION"
 
 echo "⚙️  Building $SCHEME..."
 set -o pipefail && xcodebuild $TYPE \
 -workspace $WORKSPACE \
 -scheme "$SCHEME" \
 -configuration $CONFIGURATION \
+-resultBundlePath $RESULT_BUNDLE_PATH \
 -destination platform="$PLATFORM_IOS" | $SWIFT_RUN xcbeautify --quiet
